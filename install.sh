e76888f5cbf0eb46f6869ba955f01a18c6731ab5
reyzhao <reyzhao@tencent.com>
2026-01-12 00:55:44 +0800

    update

--- install.sh
+++ install.sh
@@ -82,7 +82,9 @@ echo 'install fzf done'
 
 # install nvm for coc and gemini 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 20.18.1
 nvm use 20.18.1
 
