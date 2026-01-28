0f36e3ed647fbcc7aecedc7b47849ed910fb515f
reyzhao <reyzhao@tencent.com>
2026-01-28 23:07:38 +0800

    update

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ sudo apt install ccls -y
 sudo apt install bear -y
 sudo apt install rsync -y
 sudo apt install git -y
+sudo apt install tmux -y
 python -m pip install --upgrade trzsz
 echo 'apt install pkgs done'
 
