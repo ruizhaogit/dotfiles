5a7ff9883965344198b913a4d1cf95bf41d08be4
reyzhao <reyzhao@tencent.com>
2026-01-31 03:43:41 +0800

    update

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ mkdir -p ~/ruizhao/workspace
 cd ~/ruizhao/workspace 
 # git clone https://github.com/ruizhaogit/dotfiles
 rm -rf dotfiles-main
+rm -rf dotfiles
 curl -fLo dotfiles.zip https://github.com/ruizhaogit/dotfiles/archive/refs/heads/main.zip
 unzip dotfiles.zip
 mv dotfiles-main dotfiles
