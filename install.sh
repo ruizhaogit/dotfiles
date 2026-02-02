479fb15f18ee28986942c247a3a8318a8d4df239
ruizhao <rui.zhao.ml@gmail.com>
2026-02-02 11:50:47 +0800

    update

--- install.sh
+++ install.sh
@@ -93,6 +93,10 @@ PACKAGES=(
     "curl"
     "unzip"
     "snapd"
+    "tmux"
+    "ripgrep"
+    "ccls"
+    "bear"
 )
 
 # Array to store packages that actually need installing
@@ -122,56 +126,56 @@ fi
 
 python3 -m pip install --upgrade trzsz
 
-sudo apt update
-
-echo "Attempting to install ripgrep via apt..."
-if sudo apt install -y ripgrep; then
-    echo "ripgrep installed successfully via apt."
-else
-    echo "apt install failed. Proceeding to download the binary..."
-    # install ripgrep
-    mkdir -p ~/ruizhao/workspace/ripgrep
-    cd ~/ruizhao/workspace/ripgrep
-    curl -fLO https://github.com/BurntSushi/ripgrep/releases/download/15.1.0/ripgrep-15.1.0-aarch64-unknown-linux-gnu.tar.gz
-    tar -xvf ripgrep-15.1.0-aarch64-unknown-linux-gnu.tar.gz 
-    sudo mv ripgrep-15.1.0-aarch64-unknown-linux-gnu/rg /usr/local/bin/
-fi
-
-echo "Attempting to install tmux via apt..."
-if sudo apt install -y tmux; then
-    echo "tmux installed successfully via apt."
-else
-    echo "apt install failed. Proceeding to build from source..."
-    # install tmux
-    cd ~/ruizhao/workspace
-    curl -fLO https://github.com/tmux/tmux/releases/download/3.6a/tmux-3.6a.tar.gz
-    tar -xvzf tmux-3.6a.tar.gz
-    cd tmux-3.6a/
-    ./configure && make
-    sudo make install
-fi
-
-echo "Attempting to install ccls via apt..."
-if sudo apt install -y ccls; then
-    echo "ccls installed successfully via apt."
-else
-    echo "apt install failed. Proceeding to build from source..."
-    # install ccls
-    cd ~/ruizhao/workspace
-    curl -fLO https://github.com/MaskRay/ccls/archive/refs/tags/0.20250815.1.tar.gz
-    tar -xvzf 0.20250815.1.tar.gz
-    cd ccls-0.20250815.1/
-    cmake -S. -BRelease
-    cmake --build Release --target install
-fi
-
-echo "Attempting to install bear via apt..."
-if sudo apt install -y bear; then
-    echo "bear installed successfully via apt."
-else
-    echo "apt install failed. Proceeding to install from other pkg managers..."
-    sudo snap install bear --classic
-fi
+# sudo apt update
+#
+# echo "Attempting to install ripgrep via apt..."
+# if sudo apt install -y ripgrep; then
+#     echo "ripgrep installed successfully via apt."
+# else
+#     echo "apt install failed. Proceeding to download the binary..."
+#     # install ripgrep
+#     mkdir -p ~/ruizhao/workspace/ripgrep
+#     cd ~/ruizhao/workspace/ripgrep
+#     curl -fLO https://github.com/BurntSushi/ripgrep/releases/download/15.1.0/ripgrep-15.1.0-aarch64-unknown-linux-gnu.tar.gz
+#     tar -xvf ripgrep-15.1.0-aarch64-unknown-linux-gnu.tar.gz 
+#     sudo mv ripgrep-15.1.0-aarch64-unknown-linux-gnu/rg /usr/local/bin/
+# fi
+#
+# echo "Attempting to install tmux via apt..."
+# if sudo apt install -y tmux; then
+#     echo "tmux installed successfully via apt."
+# else
+#     echo "apt install failed. Proceeding to build from source..."
+#     # install tmux
+#     cd ~/ruizhao/workspace
+#     curl -fLO https://github.com/tmux/tmux/releases/download/3.6a/tmux-3.6a.tar.gz
+#     tar -xvzf tmux-3.6a.tar.gz
+#     cd tmux-3.6a/
+#     ./configure && make
+#     sudo make install
+# fi
+#
+# echo "Attempting to install ccls via apt..."
+# if sudo apt install -y ccls; then
+#     echo "ccls installed successfully via apt."
+# else
+#     echo "apt install failed. Proceeding to build from source..."
+#     # install ccls
+#     cd ~/ruizhao/workspace
+#     curl -fLO https://github.com/MaskRay/ccls/archive/refs/tags/0.20250815.1.tar.gz
+#     tar -xvzf 0.20250815.1.tar.gz
+#     cd ccls-0.20250815.1/
+#     cmake -S. -BRelease
+#     cmake --build Release --target install
+# fi
+#
+# echo "Attempting to install bear via apt..."
+# if sudo apt install -y bear; then
+#     echo "bear installed successfully via apt."
+# else
+#     echo "apt install failed. Proceeding to install from other pkg managers..."
+#     sudo snap install bear --classic
+# fi
 
 ## use snap to install pkgs
 # sudo snap install ripgrep --classic
