82b9436b9db01bb82ac2b4491f42abe9b61b7a92
ruizhao <rui.zhao.ml@gmail.com>
2026-02-02 11:58:56 +0800

    update

--- install.sh
+++ install.sh
@@ -191,101 +191,138 @@ python3 -m pip install --upgrade trzsz
 
 echo 'install pkgs done'
 
-# clone dotfiles
-mkdir -p ~/ruizhao/workspace
-cd ~/ruizhao/workspace 
-# git clone https://github.com/ruizhaogit/dotfiles
-rm -rf dotfiles-main
-rm -rf dotfiles
-curl -fLo dotfiles.zip https://github.com/ruizhaogit/dotfiles/archive/refs/heads/main.zip
-unzip dotfiles.zip
-mv dotfiles-main dotfiles
-
-## tmux
-# download .tmux.conf
-curl -fLo ~/.tmux.conf "https://raw.githubusercontent.com/ruizhaogit/dotfiles/refs/heads/main/rc_files/.tmux.conf"
-# download tmux plugin manager
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-curl -fLo ~/.tmux/plugins/tpm.zip --create-dirs https://github.com/tmux-plugins/tpm/archive/refs/heads/master.zip
-cd ~/.tmux/plugins/
-rm -rf tpm-master
-rm -rf tpm
-unzip tpm.zip
-mv tpm-master tpm
-# If this fails, the script continues because 'true' always succeeds
-~/.tmux/plugins/tpm/bin/install_plugins || true
-
-## build vim from source
-echo 'build vim from source'
-curl -fLo ~/ruizhao/workspace/vim.tar.gz --create-dirs "https://github.com/vim/vim/archive/refs/tags/v9.1.2077.tar.gz"
-cd ~/ruizhao/workspace
-tar -xvzf ~/ruizhao/workspace/vim.tar.gz
-cd ~/ruizhao/workspace/vim-9.1.2077
-./configure --enable-python3interp --with-python3-command=/usr/bin/python3
-make
-sudo make install
-echo 'build vim from source done'
-
-## vim
-# download .vimrc
-echo 'install vim plugins'
-curl -fLo ~/.vimrc "https://raw.githubusercontent.com/ruizhaogit/dotfiles/refs/heads/main/rc_files/.vimrc"
-# install vim plugin manager
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-curl -fLo ~/.vim/coc-settings.json --create-dirs "https://raw.githubusercontent.com/ruizhaogit/dotfiles/refs/heads/main/rc_files/coc-settings.json"
-# vim +'PlugInstall --sync' +qa
-vim -es -u ~/.vimrc +'PlugInstall --sync' +qa < /dev/null || true
-echo 'install vim plugins done'
-
-# install universal-ctags
-echo 'install universal-ctags'
-# git clone https://github.com/universal-ctags/ctags.git ~/ruizhao/workspace/ctags
-cd ~/ruizhao/workspace
-curl -fLo ctags.zip https://github.com/universal-ctags/ctags/archive/refs/heads/master.zip
-rm -rf ctags-master
-rm -rf ctags
-unzip ctags.zip
-mv ctags-master ctags
-cd ~/ruizhao/workspace/ctags
-./autogen.sh
-./configure --prefix=/usr/local
-make
-sudo make install
-echo 'install universal-ctags done'
-
-# update ~/.bashrc
-echo "set -o vi" >> ~/.bashrc
-echo "export TERM=xterm-256color" >> ~/.bashrc
-# echo 'eval "$(fzf --bash)"' >> ~/.bashrc
-
-## kmonad
-# https://github.com/kmonad/kmonad/releases
-echo 'install kmonad'
-cd ~/ruizhao/workspace
-curl -fLo ~/ruizhao/workspace/kmonad https://github.com/kmonad/kmonad/releases/download/0.4.4/kmonad
-chmod +x kmonad
-sudo cp ./kmonad /usr/bin/kmonad
-
-KBD_NAME=$(ls /dev/input/by-id | grep -- -kbd | head -n 1)
-if [ -n "$KBD_NAME" ]; then
-    sed -i "4c input  (device-file \"/dev/input/by-id/${KBD_NAME}\")" ~/ruizhao/workspace/dotfiles/kmonad/keymap/tutorial.kbd
+read -p "Download dotfiles? (y/n): " confirm < /dev/tty
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    # clone dotfiles
+    mkdir -p ~/ruizhao/workspace
+    cd ~/ruizhao/workspace 
+    # git clone https://github.com/ruizhaogit/dotfiles
+    rm -rf dotfiles-main
+    rm -rf dotfiles
+    curl -fLo dotfiles.zip https://github.com/ruizhaogit/dotfiles/archive/refs/heads/main.zip
+    unzip dotfiles.zip
+    mv dotfiles-main dotfiles
+else
+    echo "skip"
 fi
-sed -i "s|~|$HOME|" ~/ruizhao/workspace/dotfiles/kmonad/keymap/kmonad.service
 
-sudo cp ~/ruizhao/workspace/dotfiles/kmonad/keymap/kmonad.service /etc/systemd/system/kmonad.service
-echo 'install kmonad done'
+read -p "Download tmux conf and plugins? (y/n): " confirm < /dev/tty
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    ## tmux
+    # download .tmux.conf
+    curl -fLo ~/.tmux.conf "https://raw.githubusercontent.com/ruizhaogit/dotfiles/refs/heads/main/rc_files/.tmux.conf"
+    # download tmux plugin manager
+    # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    curl -fLo ~/.tmux/plugins/tpm.zip --create-dirs https://github.com/tmux-plugins/tpm/archive/refs/heads/master.zip
+    cd ~/.tmux/plugins/
+    rm -rf tpm-master
+    rm -rf tpm
+    unzip tpm.zip
+    mv tpm-master tpm
+    # If this fails, the script continues because 'true' always succeeds
+    ~/.tmux/plugins/tpm/bin/install_plugins || true
+else
+    echo "skip"
+fi
 
-# install fzf
-echo 'install fzf'
-# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-cd ~
-curl -fLo ~/.fzf.zip https://github.com/junegunn/fzf/archive/refs/heads/master.zip
-rm -rf fzf-master
-rm -rf .fzf
-unzip .fzf.zip
-mv fzf-master .fzf
-~/.fzf/install --all
-echo 'install fzf done'
+read -p "Download build vim and install plugins? (y/n): " confirm < /dev/tty
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    ## build vim from source
+    echo 'build vim from source'
+    curl -fLo ~/ruizhao/workspace/vim.tar.gz --create-dirs "https://github.com/vim/vim/archive/refs/tags/v9.1.2077.tar.gz"
+    cd ~/ruizhao/workspace
+    tar -xvzf ~/ruizhao/workspace/vim.tar.gz
+    cd ~/ruizhao/workspace/vim-9.1.2077
+    ./configure --enable-python3interp --with-python3-command=/usr/bin/python3
+    make
+    sudo make install
+    echo 'build vim from source done'
+
+    ## vim
+    # download .vimrc
+    echo 'install vim plugins'
+    curl -fLo ~/.vimrc "https://raw.githubusercontent.com/ruizhaogit/dotfiles/refs/heads/main/rc_files/.vimrc"
+    # install vim plugin manager
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+    curl -fLo ~/.vim/coc-settings.json --create-dirs "https://raw.githubusercontent.com/ruizhaogit/dotfiles/refs/heads/main/rc_files/coc-settings.json"
+    # vim +'PlugInstall --sync' +qa
+    vim -es -u ~/.vimrc +'PlugInstall --sync' +qa < /dev/null || true
+    echo 'install vim plugins done'
+else
+    echo "skip"
+fi
+
+
+read -p "Install universal-ctags? (y/n): " confirm < /dev/tty
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    # install universal-ctags
+    echo 'install universal-ctags'
+    # git clone https://github.com/universal-ctags/ctags.git ~/ruizhao/workspace/ctags
+    cd ~/ruizhao/workspace
+    curl -fLo ctags.zip https://github.com/universal-ctags/ctags/archive/refs/heads/master.zip
+    rm -rf ctags-master
+    rm -rf ctags
+    unzip ctags.zip
+    mv ctags-master ctags
+    cd ~/ruizhao/workspace/ctags
+    ./autogen.sh
+    ./configure --prefix=/usr/local
+    make
+    sudo make install
+    echo 'install universal-ctags done'
+else
+    echo "skip"
+fi
+
+
+read -p "Update bashrc? (y/n): " confirm < /dev/tty
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    # update ~/.bashrc
+    echo "set -o vi" >> ~/.bashrc
+    echo "export TERM=xterm-256color" >> ~/.bashrc
+    # echo 'eval "$(fzf --bash)"' >> ~/.bashrc
+else
+    echo "skip"
+fi
+
+read -p "Install kmonad? (y/n): " confirm < /dev/tty
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    ## kmonad
+    # https://github.com/kmonad/kmonad/releases
+    echo 'install kmonad'
+    cd ~/ruizhao/workspace
+    curl -fLo ~/ruizhao/workspace/kmonad https://github.com/kmonad/kmonad/releases/download/0.4.4/kmonad
+    chmod +x kmonad
+    sudo cp ./kmonad /usr/bin/kmonad
+
+    KBD_NAME=$(ls /dev/input/by-id | grep -- -kbd | head -n 1)
+    if [ -n "$KBD_NAME" ]; then
+        sed -i "4c input  (device-file \"/dev/input/by-id/${KBD_NAME}\")" ~/ruizhao/workspace/dotfiles/kmonad/keymap/tutorial.kbd
+    fi
+    sed -i "s|~|$HOME|" ~/ruizhao/workspace/dotfiles/kmonad/keymap/kmonad.service
+
+    sudo cp ~/ruizhao/workspace/dotfiles/kmonad/keymap/kmonad.service /etc/systemd/system/kmonad.service
+    echo 'install kmonad done'
+else
+    echo "skip"
+fi
+
+read -p "Install fzf? (y/n): " confirm < /dev/tty
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    # install fzf
+    echo 'install fzf'
+    # git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    cd ~
+    curl -fLo ~/.fzf.zip https://github.com/junegunn/fzf/archive/refs/heads/master.zip
+    rm -rf fzf-master
+    rm -rf .fzf
+    unzip .fzf.zip
+    mv fzf-master .fzf
+    ~/.fzf/install --all
+    echo 'install fzf done'
+else
+    echo "skip"
+fi
 
 # git config
 git config --global pager.color false
@@ -296,21 +333,31 @@ git config --global mergetool.fugitive.cmd 'vim -f -c "Gvdiffsplit!" "$MERGED"'
 git config --global merge.tool fugitive
 git config --global mergetool.keepBackup false
 
-# install nvm for coc and gemini 
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-cd ~/ruizhao/workspace
-curl -fLo ~/ruizhao/workspace/nvm.tar.gz --create-dirs "https://github.com/nvm-sh/nvm/archive/refs/tags/v0.40.3.tar.gz"
-tar -xvzf ~/ruizhao/workspace/nvm.tar.gz
-cd ~/ruizhao/workspace/nvm-0.40.3
-cp -r ~/ruizhao/workspace/nvm-0.40.3 ~/.nvm
-cd ~/.nvm
-\. ~/.nvm/nvm.sh
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.bashrc
-echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.bashrc
-nvm install 20.18.1
-nvm use 20.18.1
-npm install -g @google/gemini-cli
+read -p "Install nvm and nodejs? (y/n): " confirm < /dev/tty
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    # install nvm for coc and gemini 
+    # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+    cd ~/ruizhao/workspace
+    curl -fLo ~/ruizhao/workspace/nvm.tar.gz --create-dirs "https://github.com/nvm-sh/nvm/archive/refs/tags/v0.40.3.tar.gz"
+    tar -xvzf ~/ruizhao/workspace/nvm.tar.gz
+    cd ~/ruizhao/workspace/nvm-0.40.3
+    cp -r ~/ruizhao/workspace/nvm-0.40.3 ~/.nvm
+    cd ~/.nvm
+    \. ~/.nvm/nvm.sh
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
+    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.bashrc
+    echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.bashrc
+    nvm install 20.18.1
+    nvm use 20.18.1
+else
+    echo "skip"
+
+read -p "Install gemini? (y/n): " confirm < /dev/tty
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    npm install -g @google/gemini-cli
+else
+    echo "skip"
+fi
