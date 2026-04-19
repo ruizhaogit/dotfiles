35d9dc13c4439eaf0461e3220f784c6956f18fc7
Rui.Zhao <rui.zhao.ml@gmail.com>
2026-04-19 20:53:02 +0800

    update

--- install.sh
+++ install.sh
@@ -311,6 +311,12 @@ if [[ "$confirm" =~ ^[Yy]$ ]]; then
     echo "export TERM=xterm-256color" >> ~/.bashrc
     # echo 'eval "$(fzf --bash)"' >> ~/.bashrc
     echo '[ -f ~/.fzf.bash ] && source ~/.fzf.bash' >> ~/.bashrc
+    # show git branch in bash
+    echo 'parse_git_branch() {
+         git branch 2> /dev/null | sed -e '\''/^[^*]/d'\'' -e '\''s/* \(.*\)/(\1)/'\''
+    }
+    export PS1="\u@\h \w \[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "' >> ~/.bashrc
+
 fi
 
 read -p "Install kmonad? (y/n): " confirm < /dev/tty
